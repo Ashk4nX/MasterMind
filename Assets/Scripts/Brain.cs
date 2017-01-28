ca4fe1bb60f75f86ab249df24903518704517348
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-01-28 13:12:18 +0330

    First Commit
    Main gameplay setting created. Added some good plugins.

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Brain : MonoBehaviour {
+
+	public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+
+		DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Move ();
+
+	}
+
+	void Move (){
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+
+			Player.DOMove(new Vector3(-2, 0, 0), 1).SetRelative().SetLoops(0, LoopType.Restart);
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+
+			Player.DOMove(new Vector3(2, 0, 0), 1).SetRelative().SetLoops(0, LoopType.Restart);
+
+		}
+
+	}
+}
